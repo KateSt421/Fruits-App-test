2ae9996f7a76c0f68f4154022102fd8fecdc09a3
KateSt421 <belporibet@mail.ru>
2025-08-06 15:03:07 +0300

    fix: change config, delete base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Fruits-App-test/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
