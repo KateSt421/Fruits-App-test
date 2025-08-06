4c5858e3ec18e50db4b2606444f884000c8f1590
KateSt421 <belporibet@mail.ru>
2025-08-06 14:22:19 +0300

    fix: change config, url api

--- src/api/fruitsApi.ts
+++ src/api/fruitsApi.ts
@@ -1,39 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-// import type { Fruit } from './types';
+import type { Fruit } from './types';
 
 export const fruitsApi = createApi({
   reducerPath: 'fruitsApi',
-
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.PROD
-      ? 'https://katest421.github.io/Fruits-App-test/api'
-      : '/api',
+    baseUrl: 'https://www.fruityvice.com/api',
   }),
-  //   endpoints: (builder) => ({
-  //     getAllFruits: builder.query<Fruit[], void>({
-  //       query: () => '/fruit/all',
-  //       transformResponse: (response: Fruit[]) => {
-  //         return response.map(fruit => ({
-  //           ...fruit,
-  //           id: fruit.id,
-  //           isUserCreated: false
-  //         }));
-  //       }
-  //     }),
-  //     getFruitById: builder.query<Fruit, number>({
-  //       query: (id) => `fruit/${id}`,
-  //     }),
-  //   }),
-  // });
   endpoints: (builder) => ({
-    getAllFruits: builder.query({
-      query: () => 'fruit/all',
+    getAllFruits: builder.query<Fruit[], void>({
+      query: () => '/fruit/all',
     }),
-    getFruitById: builder.query({
-      query: (id) => `fruit/${id}`,
+    getFruitById: builder.query<Fruit, number>({
+      query: (id) => `/fruit/${id}`,
     }),
   }),
-})
+});
 
 export const { useGetAllFruitsQuery, useGetFruitByIdQuery } = fruitsApi;
-
